c707b6a4a53537e16b51d82c4b6de5dde4837262
Slava <sla.va@icloud.com>
2019-11-29 00:23:24 +0300

    dz tovars list

--- tovars.py
+++ tovars.py
@@ -1,10 +1,18 @@
 import collections
-tovars_list =input("Enter tovars that you bought and their prices(use '-'):")
+tovars_list =input("Enter tovars that you bought:")
+price_list =input("Enter prices of all tovars:")
 my_sum = 0
 def UniqueData(tovars_list):
     new_list = tovars_list.split(' ')
     print("Your tovars list:",new_list)
-    unigue_tovars = collections.Counter(new_list)
+    new_price_list = price_list.split(" ")
+    i = 0
+    final_list = []
+    for element in new_list:
+        temporary_list = new_list[i]+ '-' + new_price_list[i]
+        final_list.append(temporary_list)
+        i +=1
+    unigue_tovars = collections.Counter(final_list)
     return list(unigue_tovars)
 my_unique_data = UniqueData(tovars_list)
 print("Your unique Tovars:",my_unique_data)
